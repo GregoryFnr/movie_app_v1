edbd09ce0fd98d9906b8d998c0f2d762cfea332c
GregoryFnr <gregory.fnr@gmail.com>
2025-03-02 18:37:49 +0100

    Added the vueuse module for debounce

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     movieKey: process.env.MOVIE_API_KEY,
   },
+  modules: ["@vueuse/nuxt"],
   plugins: ["~/plugins/fontawesome.js"],
   css: ["@fortawesome/fontawesome-svg-core/styles.css"],
 });
